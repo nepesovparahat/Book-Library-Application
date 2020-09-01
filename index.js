08545b281081a5d4729afd2f25282222f3ab1064
Parahat Nepesov <parahatnepesov1993.18.04@gmail.com>
2020-09-01 19:10:36 +0300

    I have added the message functions that the library application displays while performing the book deletion and operations.

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ class user_interface{
         <td>${book.bookname}</td>
         <td>${book.bookauthor}</td>
         <td>${book.bookisbn}</td>
-        <td><a href="#" class="delete">&times</a></td>
+        <td><a href="#" class="delete">X</a></td>
         `;
         tabl.appendChild(rows);
     }
-    bookremove(){
-
+    bookremove(target){
+if(target.className==="delete"){
+    target.parentNode.parentNode.remove();
+}
     }
     bookcount(){
     const tabl=document.getElementById("booklist");
@@ -29,7 +31,16 @@ class user_interface{
     time_out(){
 
     }
-    message_show(){
+    message_show(message,className){
+        const message_box=document.createElement("div");
+        message_box.className=`alert ${className}`;
+        message_box.appendChild(document.createTextNode(message));
+        const container=document.querySelector(".container");
+        const form=document.querySelector("#bookform");
+        container.insertBefore(message_box,form);
+        setTimeout(()=>{
+            document.querySelector(".alert").remove();
+        },2000);
 
     }
     formclear(){
@@ -50,14 +61,15 @@ document.getElementById("bookform").addEventListener("submit",function(e){
     if(bookname!="" && bookauthor!="" && bookisbn!=""){
         u_interface.bookadd(book);
         u_interface.bookcount();
-       // document.getElementById("result").innerHTML="Successfully";
+        u_interface.message_show('Book successfully added','success');
+     
         u_interface.formclear();
     }
     else{
         
 
-            document.getElementById("result").innerHTML="Please do not leave blank fields!!!";
-        
+            
+        u_interface.message_show('Please fill in all fields!','error');
         
         
     }
@@ -66,5 +78,9 @@ document.getElementById("bookform").addEventListener("submit",function(e){
 });
 
 document.getElementById("booklist").addEventListener("click",function(e){
+    const u_interface=new user_interface();
+    u_interface.bookremove(e.target);
+    u_interface.bookcount();
+    u_interface.message_show('Book deleted successfully','success');
 e.preventDefault();
 });
